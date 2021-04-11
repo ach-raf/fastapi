721ed60fbdd46ff042f1fac0dc08f92b7d29b671
achraf.zouchou <achraf.zouchou@gmail.com>
2021-04-11 20:42:30 +0000

    test

--- app/main.py
+++ app/main.py
@@ -9,7 +9,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    //return {"HOST": f'{socket.gethostbyname(socket.gethostname())}'}
+    return {"HOST": f'IP'}
 
 
 @app.get("/items/{item_id}")
