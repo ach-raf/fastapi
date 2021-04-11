f535e4ada049ecc52b46e1b853f615d02f97eefc
achraf.zouchou <achraf.zouchou@gmail.com>
2021-04-11 21:41:33 +0000

    path fix test

--- app/main.py
+++ app/main.py
@@ -14,7 +14,13 @@ def read_json_file(filename):
     with open(filename) as json_file:
         data = json.load(json_file)
     return data
-         
+
+
+def imdb_data():         
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    imdb_json = os.path.join(dir_path, 'data', 'imdb.json')
+    data = read_json_file(imdb_json)
+    return data
 
 app = FastAPI()
 
@@ -26,10 +32,13 @@ def read_root():
 
 @app.get("/imdb")
 def read_imdb():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    imdb_json = f'{dir_path}\\data\\imdb.json'
-    data = read_json_file(imdb_json)
-    return data
+    return imdb_data()
+
+
+@app.get("/imdb/{imdb_id}")
+def read_imdb_item(imdb_id: str):
+    new_dict = dict((item['imdb_id'], item) for item in imdb_data())
+    return new_dict[imdb_id]
 
 
 @app.get("/items/{item_id}")
